e05d68a9756e92787a26e42dae6df050f297a858
Connor <179545997+thimbleforth@users.noreply.github.com>
2025-11-08 22:34:56 -0700

    Create DailyTools.ps1

---
+++ DailyTools.ps1
@@ -0,0 +1,9 @@
+﻿<# Powershell tools I use often #>
+
+<# Battery checker and report generator #>
+
+function Get-PowerReport() {
+    powercfg /batteryreport /output .\"$(Get-Date -Format yyyyMMdd) - batteryreport.html"    
+}
+
+Get-PowerReport
